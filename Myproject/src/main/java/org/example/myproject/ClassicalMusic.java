7369399ad5a4687a2323c3b63980af7b48100614
Nikita <melianas@student.21-school.ru>
2023-07-24 02:01:56 +0300

    Rock and Classical music IoC

---
+++ Myproject/src/main/java/org/example/myproject/ClassicalMusic.java
@@ -0,0 +1,7 @@
+package org.example.myproject;
+
+public class ClassicalMusic implements Music {
+    public String getSong() {
+        return "Hungarian Rapsody";
+    }
+}
---
+++ Myproject/src/main/java/org/example/myproject/Music.java
@@ -0,0 +1,5 @@
+package org.example.myproject;
+
+public interface Music {
+    String getSong();
+}
