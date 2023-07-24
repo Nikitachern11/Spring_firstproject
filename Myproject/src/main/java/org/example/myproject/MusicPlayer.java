3cf2f9b2cafe99e6212b45914b76218f19fd0611
Nikita <melianas@student.21-school.ru>
2023-07-24 18:38:34 +0300

    list of beans

--- Myproject/src/main/java/org/example/myproject/MusicPlayer.java
+++ Myproject/src/main/java/org/example/myproject/MusicPlayer.java
@@ -1,23 +1,23 @@
 package org.example.myproject;
 
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList;
     private int volume;
     private String name;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {
     }
 
     public void playMusic() {
-        System.out.println("Playing " + music.getSong());
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+        for (Music music : musicList) {
+            System.out.println(music.getSong());
+        }
     }
 
     public int getVolume() {
@@ -35,4 +35,6 @@ public class MusicPlayer {
     public void setName(String name) {
         this.name = name;
     }
+
+
 }
