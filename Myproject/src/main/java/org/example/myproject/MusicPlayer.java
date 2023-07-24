65d22547dc1fdca0807883ce3356c0a203e4d92b
Nikita <melianas@student.21-school.ru>
2023-07-24 16:27:05 +0300

    properties from file

--- Myproject/src/main/java/org/example/myproject/MusicPlayer.java
+++ Myproject/src/main/java/org/example/myproject/MusicPlayer.java
@@ -2,11 +2,37 @@ package org.example.myproject;
 
 public class MusicPlayer {
     private Music music;
+    private int volume;
+    private String name;
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public MusicPlayer() {
+    }
+
     public void playMusic() {
         System.out.println("Playing " + music.getSong());
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- Myproject/src/main/java/org/example/myproject/TestSpring.java
+++ Myproject/src/main/java/org/example/myproject/TestSpring.java
@@ -7,6 +7,7 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName() + " " + musicPlayer.getVolume());
         context.close();
     }
 }
